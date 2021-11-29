bbfb96321e7a62792654a5b9e6cf37ee0a55efea
Luk4shenko <zushdesign@yandex.ru>
2021-11-29 10:38:44 -0800

    Prac2

---
+++ Prac2 Lukashenko/src/TestAuthor.java
@@ -0,0 +1,9 @@
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author a1 = new Author("Sue Grant", 'm', "sugrant@somewhere.com");
+        System.out.println(a1);
+        a1.setEmail("sgrant@gmail.com");
+        System.out.println(a1);
+
+    }
+}
